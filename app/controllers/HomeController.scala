7d8c5965cc3f8a15ab9034d298da91c97915af48
gilcu2 <gilcu2@gmail.com>
2017-04-12 19:50:55 +0200

    catalog working

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,15 +1,20 @@
 package controllers
 
 import javax.inject._
+
 import play.api._
 import play.api.mvc._
+import models.Product
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.i18n.Messages.Implicits._
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
+
 @Singleton
-class HomeController @Inject() extends Controller {
+class HomeController @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
   /**
    * Create an Action to render an HTML page.
@@ -18,10 +23,14 @@ class HomeController @Inject() extends Controller {
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
+
   def index = Action { implicit request =>
     Ok(views.html.index())
   }
 
-  def todo = TODO
+  def list = Action { implicit request =>
+    val products = Product.findAll
+    Ok(views.html.products.list(products))
+  }
 
 }
